1a8eecd43f5837e4a34893b1858c3069756cc98c
Mandi Lee <github@mandilee.co.uk>
2021-12-08 08:58:57 +0000

    Update EpicRPG.js

--- EpicRPG/EpicRPG.js
+++ EpicRPG/EpicRPG.js
@@ -1,28 +1,31 @@
 function EpicRPG(message) {
   
-  // Add a warning heart react if HP drops below 30%
-  // Add a warning triangle if (lost HP*2) > remaining HP
-  if (message.content.indexOf('remaining HP is') > 0) {
-    let str = message.content.split('HP, remaining HP is');
-    let lostHp = str[0].split('Lost');
-    let health = str[1].split('/'),
-        fullhealth = health[1].split('*');
-    if ((health[0] / fullhealth[0]) < 0.3) { message.react("❣️"); }
-    if ((lostHp[1] * 2) > health[0]) { message.react("⚠️"); }
-  }
-
-  // Add RIP when beating miniboss
-  if (message.content.indexOf('HAS BEEN DEFEATED') > 0) {
-    message.react("602574893745307668");
-  }
-
-  // 
-  if (message.content.indexOf('DRAGON DIED,') > 0) {
-    message.react("🥳");
-  }
-
+  this.message = message;
   
-}
+  this.check = function() {
+  
+    // Add a warning heart react if HP drops below 30%
+    // Add a warning triangle if (lost HP*2) > remaining HP
+    if (this.message.content.indexOf('remaining HP is') > 0) {
+      let str = this.message.content.split('HP, remaining HP is');
+      let lostHp = str[0].split('Lost');
+      let health = str[1].split('/'),
+          fullhealth = health[1].split('*');
+      if ((health[0] / fullhealth[0]) < 0.3) { this.message.react("❣️"); }
+      if ((lostHp[1] * 2) > health[0]) { this.message.react("⚠️"); }
+    }
 
+    // Add RIP when beating miniboss
+    if (this.message.content.indexOf('HAS BEEN DEFEATED') > 0) {
+      this.message.react("602574893745307668");
+    }
+
+    // Party when beating the dragon
+    if (this.message.content.indexOf('DRAGON DIED,') > 0) {
+      mesthis.messagesage.react("🥳");
+    }
+
+  }
+}
     
 module.exports = { EpicRPG }
